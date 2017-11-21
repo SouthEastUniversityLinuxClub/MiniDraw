9597522a69c8137cf246683e1dd7ce0d2ab64582
ZizhuoWang <wjwzzc@163.com>
2017-11-21 09:16:23 +0800

    Removed unnecessary files

--- MiniDrawPacking/lib.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/sh  
-      
-exe="MiniDraw" #发布的程序名称  
-des="/home/wang/Documents/Qt/MiniDrawPacking" #你的路径  
-      
-deplist=$(ldd $exe | awk  '{if (match($3,"/")){ printf("%s "),$3 } }')  
-cp $deplist $des  
